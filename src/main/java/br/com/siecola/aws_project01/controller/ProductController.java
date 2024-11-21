90b81182ced17b40871a46e368ec61cafb0e5600
Mateus Pereira Jorge <mateus.p.jorge@gmail.com>
2024-11-21 11:01:43 -0300

    atualizando codigo conforme as aulas do curso

--- src/main/java/br/com/siecola/aws_project01/controller/ProductController.java
+++ src/main/java/br/com/siecola/aws_project01/controller/ProductController.java
@@ -1,7 +1,9 @@
 package br.com.siecola.aws_project01.controller;
 
+import br.com.siecola.aws_project01.enums.EventType;
 import br.com.siecola.aws_project01.model.Product;
 import br.com.siecola.aws_project01.repository.ProductRepository;
+import br.com.siecola.aws_project01.service.ProductPublisher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +15,13 @@ import java.util.Optional;
 @RequestMapping("/api/products")
 public class ProductController {
 
-    private final ProductRepository productRepository;
+    private ProductRepository productRepository;
+    private ProductPublisher productPublisher;
 
     @Autowired
-    public ProductController(ProductRepository productRepository) {
+    public ProductController(ProductRepository productRepository, ProductPublisher productPublisher) {
         this.productRepository = productRepository;
+        this.productPublisher = productPublisher;
     }
 
     @GetMapping
@@ -28,21 +32,36 @@ public class ProductController {
     @GetMapping("/{id}")
     public ResponseEntity<Product> findById(@PathVariable long id) {
         Optional<Product> optProduct = productRepository.findById(id);
-        return optProduct.map(product -> new ResponseEntity<>(product, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        if (optProduct.isPresent()) {
+            return new ResponseEntity<Product>(optProduct.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @PostMapping
-    public ResponseEntity<Product> saveProduct(@RequestBody Product product) {
+    public ResponseEntity<Product> saveProduct(
+            @RequestBody Product product) {
         Product productCreated = productRepository.save(product);
-        return new ResponseEntity<Product>(productCreated, HttpStatus.CREATED);
+
+        productPublisher.publishProductEvent(productCreated, EventType.PRODUCT_CREATED, "matilde");
+
+        return new ResponseEntity<Product>(productCreated,
+                HttpStatus.CREATED);
     }
 
     @PutMapping(path = "/{id}")
-    public ResponseEntity<Product> updateProduct(@RequestBody Product product, @PathVariable("id") long id) {
+    public ResponseEntity<Product> updateProduct(
+            @RequestBody Product product, @PathVariable("id") long id) {
         if (productRepository.existsById(id)) {
             product.setId(id);
+
             Product productUpdated = productRepository.save(product);
-            return new ResponseEntity<Product>(productUpdated, HttpStatus.OK);
+
+            productPublisher.publishProductEvent(productUpdated, EventType.PRODUCT_UPDATE, "doralice");
+
+            return new ResponseEntity<Product>(productUpdated,
+                    HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
@@ -53,7 +72,11 @@ public class ProductController {
         Optional<Product> optProduct = productRepository.findById(id);
         if (optProduct.isPresent()) {
             Product product = optProduct.get();
+
             productRepository.delete(product);
+
+            productPublisher.publishProductEvent(product, EventType.PRODUCT_DELETED, "hannah");
+
             return new ResponseEntity<Product>(product, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -63,7 +86,11 @@ public class ProductController {
     @GetMapping(path = "/bycode")
     public ResponseEntity<Product> findByCode(@RequestParam String code) {
         Optional<Product> optProduct = productRepository.findByCode(code);
-        return optProduct.map(product -> new ResponseEntity<>(product, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        if (optProduct.isPresent()) {
+            return new ResponseEntity<Product>(optProduct.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
 }
